6acfbac971dfad1fd5d6fb1bb5916d120625eb3c
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-28 08:34:23 -0800

    learning about the different types

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log('It works!');
+var myName = 'Steph';
+var age = 29;
+var hasHobbies = true;
+var realAge;
+realAge = 28;
+var hobbies = ['Cooking', 'Sports'];
+hobbies = [100];
+var address = ['string', 0];
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
 console.log('It works!');
+
+const myName = 'Steph';
+const age = 29;
+const hasHobbies = true;
+
+let realAge: number;
+realAge = 28;
+
+let hobbies: any[] = ['Cooking', 'Sports'];
+hobbies = [100];
+
+const address: [string, number] = ['string', 0];
+
+enum Color {
+  Gray,
+  Green,
+  Blue
+}
+
+const myColor: Color = Color.Green;
+console.log(myColor);
